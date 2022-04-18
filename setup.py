7149f07899e17ce8aae06fdf687f675cda448075
housenli <54141572+housenli@users.noreply.github.com>
2022-04-19 00:41:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setup(
     description='Python implementation of Multiscale Nemirovski Dantzig Estimators',
     packages=find_packages(),
     package_dir={'pymind': 'pymind'},
-    author={'Leo Claus Weber', 'Housen Li'}
+    author={'Leo Claus Weber', 'Housen Li'},
     install_requires=[require]
 )
